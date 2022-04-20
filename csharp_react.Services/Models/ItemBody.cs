350a0baaca1f9b341fba5f0845eb8d0348b6384c
Angel Gutierrez <agutierrez01@manhattan.edu>
2022-04-20 16:08:33 -0400

    removing .net from project

--- csharp_react.Services/Models/ItemBody.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace csharp_react.Services.Models
-{
-    public class ItemBody
-    {
-        public string Name { get; set; }
-        public string Price { get; set; }
-        public int Quantity { get; set; }
-    }
-}
--- csharp_react.Services/Models/LoginBody.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace csharp_react.Services.Models
-{
-    public class LoginBody
-    {
-        public int EmployeeId { get; set; }
-        public int Passcode { get; set; }
-    }
-}
